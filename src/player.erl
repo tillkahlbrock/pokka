9b905453b6431ebfce780697addc48561d6b57bb
Till Kahlbrock <till.kahlbrock@gmx.com>
2012-12-17 16:13:33 +0100

    added pocket card dealing

--- src/player.erl
+++ src/player.erl
@@ -1,12 +1,22 @@
 -module(player).
--export([join/2]).
+-export([join/2, bet/1]).
 
 join(TablePid, Name) ->
   MRef = make_ref(),
   TablePid ! {self(), MRef, {join, Name}},
   receive
-    {MRef, ok} -> ok
+    {MRef, ok} -> ok,
+    PocketCards = receive_pocket_cards(),
+    PocketCards
   after 2000 ->
     ptimeout
   end.
   
+receive_pocket_cards() ->
+  receive
+    {pocket_cards, PocketCards} ->
+      PocketCards
+  end.
+
+bet(Cards) ->
+  io:format("got cards: ~p~n", [Cards]).
--- src/table.erl
+++ src/table.erl
@@ -7,15 +7,26 @@
   }).
 
 init() ->
-  loop(#state{players=[]}).
+  timer:send_after(20000, {join_over}),
+  join_loop(#state{players=[]}).
 
-loop(State) ->
+join_loop(State) ->
   receive
     {Pid, MRef, {join, Name}} ->
       Players = State#state.players,
       NewState = State#state{players=[#player{name=Name, pid=Pid}|Players]},
       Pid ! {MRef, ok},
-      loop(NewState);
+      join_loop(NewState);
+    {join_over} ->
+      play_loop(State);
     {kill} ->
       exit(normal)
   end.
+
+play_loop(State) ->
+  send_cards(State#state.players).
+  
+send_cards([]) -> true; % Jump to betting loop...
+send_cards([Player|Rest]) ->
+  Player#player.pid ! {pocket_cards, {dh,kk}},
+  send_cards(Rest).
