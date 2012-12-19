6f185f1d86a7a4295f278dc66f90409fc5493023
Till Kahlbrock <till@jimdo.com>
2012-12-19 08:39:33 +0100

    rewrite table and player to use gen_server

--- src/player.erl
+++ src/player.erl
@@ -1,21 +1,6 @@
 -module(player).
--export([join/2, wait_for_pocket_cards/0]).
+-export([join/2, close_table/1]).
 
-join(TablePid, Name) ->
-  MRef = make_ref(),
-  TablePid ! {self(), MRef, {join, Name}},
-  receive
-    {MRef, ok} -> ok
-  after 3000 ->
-    exit(timeout_value)
-  end.
+join(TablePid, Name) -> gen_server:call(TablePid, {join, Name}).
 
-wait_for_pocket_cards() ->
-  receive
-    {Table, MRef, {pocket_cards, Cards}} ->
-      Table ! {MRef, ok},
-      Cards
-  after 30000 ->
-    exit(timeout_value)
-  end.
-
+close_table(TablePid) -> gen_server:call(TablePid, terminate).
--- src/table.erl
+++ src/table.erl
@@ -1,40 +1,28 @@
 -module(table).
--export([init/0]).
+-behaviour(gen_server).
+-export([start/0, start_link/0]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 -record(state, {players=[]}).
--record(player, {
-  name,
-  pid
-  }).
+-record(player, {name, pid}).
 
-init() ->
-  timer:send_after(20000, {join_over}),
-  join_loop(#state{players=[]}).
+start() -> gen_server:start(?MODULE, #state{}, []).
 
-join_loop(State) ->
-  receive
-    {Pid, MRef, {join, Name}} ->
-      Players = State#state.players,
-      NewState = State#state{players=[#player{name=Name, pid=Pid}|Players]},
-      Pid ! {MRef, ok},
-      join_loop(NewState);
-    {join_over} ->
-      play_loop(State);
-    {kill} ->
-      exit(normal)
-  end.
+start_link() -> gen_server:start_link(?MODULE, #state{}, []).
 
-play_loop(State) ->
-  send_cards(State#state.players).
+init(State) -> {ok, State}.
 
-send_cards([]) -> true; % Jump to betting loop...
-send_cards([Player|Rest]) ->
-  MRef = make_ref(),
-  Player#player.pid ! {self(), MRef, {pocket_cards, {dh,kk}}},
-  receive
-    {MRef, ok} ->
-      send_cards(Rest)
-  after 2000 ->
-    mark_inactive(Player)
-  end.
+handle_call({join, Name}, From, State) ->
+  Players = State#state.players,
+  Player = #player{name=Name, pid=From},
+  NewState = State#state{players=[Player|Players]},
+  {reply, ok, NewState};
 
-mark_inactive(_Player) -> true. % Not yet implemented ;)
+handle_call(terminate, _From, State) -> {stop, normal, ok, State}.
+
+handle_cast(_Message, State) -> {noreply, State}.
+
+handle_info(_Message, State) -> {noreply, State}.
+
+terminate(normal, State) -> io:format("shutting down. state: ~p~n", [State]).
+
+code_change(_OldVersion, State, _Extra) -> {ok, State}.
