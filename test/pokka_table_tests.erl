b35e23b91af519e6516d3a5987cea2659ef6b10b
Till Kahlbrock <till.kahlbrock@gmx.com>
2012-12-26 20:05:20 +0100

    fixes broken unit tests after module renaming

--- test/pokka_table_tests.erl
+++ test/pokka_table_tests.erl
@@ -8,16 +8,16 @@ handle_call_join_should_add_the_player_to_the_empty_players_list_test_() ->
   From = some_pid,
   InitialState = #state{players=[]},
   NewState = #state{players=[#player{name=Name,pid=From}]},
-  [?_assertEqual({reply, ok, NewState}, table:handle_call({join, Name}, From, InitialState))].
+  [?_assertEqual({reply, ok, NewState}, pokka_table:handle_call({join, Name}, From, InitialState))].
 
 handle_call_terminate_should_return_a_stop_normal_signal_test_() ->
-  [?_assertEqual({stop, normal, ok, state}, table:handle_call(terminate, from, state))].
+  [?_assertEqual({stop, normal, ok, state}, pokka_table:handle_call(terminate, from, state))].
 
 handle_cast_should_ignore_any_casts_test_() ->
-  [?_assertEqual({noreply, state},  table:handle_cast(message, state))].
+  [?_assertEqual({noreply, state},  pokka_table:handle_cast(message, state))].
 
 handle_info_should_ignore_all_unknown_messages_test_() ->
-  [?_assertEqual({noreply, state},  table:handle_info(message, state))].
+  [?_assertEqual({noreply, state},  pokka_table:handle_info(message, state))].
 
 code_change_should_do_nothing_test_() ->
-  [?_assertEqual({ok, state},  table:code_change(oldversion, state, extra))].
+  [?_assertEqual({ok, state},  pokka_table:code_change(oldversion, state, extra))].
