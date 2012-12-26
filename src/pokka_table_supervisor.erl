cbfd7258a23ece405c6f72dd9492eaedb354eccb
Till Kahlbrock <till.kahlbrock@gmx.com>
2012-12-26 20:03:19 +0100

    adds a first idea for the table supervisor

---
+++ src/pokka_table_supervisor.erl
@@ -0,0 +1,21 @@
+-module(pokka_table_supervisor).
+-behavior(supervisor).
+-export([start_link/0]).
+-export([init/1]).
+
+
+start_link() ->
+  supervisor:start_link({local,?MODULE}, ?MODULE, normal).
+
+init(normal) ->
+  {ok, {
+    {one_for_all, 5, 60},
+    [
+      {pokka_table,
+      {pokka_table, start_link, []},
+      permanent,
+      5000,
+      worker,
+      [pokka_table]}
+    ]}
+  }.
