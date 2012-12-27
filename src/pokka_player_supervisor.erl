072940c8ec1bd36b85da23ab20cad05c902e313b
Till Kahlbrock <till.kahlbrock@gmx.com>
2012-12-27 15:56:13 +0100

    adds player supervisor

---
+++ src/pokka_player_supervisor.erl
@@ -0,0 +1,24 @@
+-module(pokka_player_supervisor).
+-behaviour(supervisor).
+-export([start_link/0, start_player/3, stop_player/1]).
+-export([init/1]).
+
+start_link() ->
+  supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+init([]) ->
+  MaxRestart = 6,
+  MaxTime = 3600,
+  {ok, {{one_for_one, MaxRestart, MaxTime}, []}}.
+
+start_player(Name) ->
+  ChildSpec = {
+    Name,
+    {pokka_player, start_link, [Name]},
+    permanent, 10500, worker, [pokka_player]
+  },
+  supervisor:start_child(?MODULE, ChildSpec).
+
+stop_player(Name) ->
+  supervisor:terminate_child(?MODULE, Name),
+  supervisor:delete_child(?MODULE, Name).
