a9f9faaca18bb9179933fb903ebef6efe47ce769
Till Kahlbrock <till.kahlbrock@gmx.com>
2013-01-05 21:57:35 +0100

    adds simple timeout to idle state

--- src/pokka.erl
+++ src/pokka.erl
@@ -9,9 +9,7 @@ stop(_State) ->
   ok.
 
 join_table(Table, Name, PlayerPid) ->
-  ok = gen_fsm:sync_send_event(Table, {join, Name, PlayerPid}),
-  ok.
+  gen_fsm:send_event(Table, {join, Name, PlayerPid}).
 
 leave_table(Table, Name, PlayerPid) ->
-  ok = gen_fsm:send_all_state_event(Table, {leave, Name, PlayerPid}),
-  ok.
+  gen_fsm:send_all_state_event(Table, {leave, Name, PlayerPid}).
--- src/pokka_table.erl
+++ src/pokka_table.erl
@@ -2,7 +2,7 @@
 -behaviour(gen_fsm).
 -export([start/0, start_link/1]).
 -export([init/1, handle_event/3, handle_sync_event/4, handle_info/3, terminate/3, code_change/4]).
--export([idle/3]).
+-export([idle/2]).
 -record(state, {players=[]}).
 -record(player, {name, pid}).
 
@@ -12,12 +12,16 @@ start_link(Table) -> gen_fsm:start_link({local,Table}, ?MODULE, #state{}, []).
 
 init(State) -> {ok, idle, State}.
 
-idle({join, Name, Pid}, _From, State) ->
+idle({join, Name, Pid}, State) ->
   Players = State#state.players,
   Player = #player{name=Name, pid=Pid},
   NewState = State#state{players=[Player|Players]},
   send_all(Players, "status: new player " ++ atom_to_list(Name) ++ " joined the table"),
-  {reply, ok, idle, NewState}.
+  {next_state, idle, NewState, 10000};
+
+idle(timeout, StateData) ->
+  io:format("timeout received. changing to game state"),
+  {next_state, game, StateData}.
 
 handle_event({leave, Name, Pid}, StateName, StateData) ->
   Player = #player{name=Name, pid=Pid},
