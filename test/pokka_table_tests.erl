4232962e7404be74afcdc9314e5abe5035fa031e
Till Kahlbrock <till.kahlbrock@gmx.com>
2013-01-07 14:56:14 +0100

    adds test for sending notification on join

--- test/pokka_table_tests.erl
+++ test/pokka_table_tests.erl
@@ -12,18 +12,21 @@ join_new_player_test_() ->
     "of the new player to the state",
     ?setup(#state{}, first_join_in_idle_state)},
     {"second player joins in idle state leads to player list with two players",
-    ?setup(#state{players=[{player1, ?SOME_PID}]}, second_join_in_idle_state)}
+    ?setup(#state{players=[{player1, ?SOME_PID}]}, second_join_in_idle_state)},
+    {"it should send a notification to the player when a new player joins",
+    ?setup(#state{players=[{player1, ?SOME_PID}]}, send_notification_to_players_on_join)}
   ].
 
 %%%%%%%%%%%%%%%%%%%%%%%
 %%% SETUP FUNCTIONS %%%
 %%%%%%%%%%%%%%%%%%%%%%%
 start(InitStateData) ->
-  pokka_table:start_link(tablename),
+  code:unstick_mod(gen_fsm),
+  meck:new(gen_fsm, [passthrough]),
   InitStateData.
 
 stop(_InitStateData) ->
-  gen_fsm:sync_send_all_state_event(tablename, terminate).
+  meck:unload(gen_fsm).
 
 %%%%%%%%%%%%%%%%%%%%
 %%% ACTUAL TESTS %%%
@@ -39,3 +42,9 @@ second_join_in_idle_state(InitStateData) ->
   Player = {player2, ?SOME_PID},
   {_, _, StateData, _} = pokka_table:idle({join, Player}, InitStateData),
   [?_assertEqual([Player, {player1, ?SOME_PID}], StateData#state.players)].
+
+send_notification_to_players_on_join(InitStateData) ->
+  meck:expect(gen_fsm, send_all_state_event, fun(Pid, Message) -> {ok, Pid, Message} end),
+  Player = {player2, ?SOME_PID},
+  pokka_table:idle({join, Player}, InitStateData),
+  [?_assert(meck:validate(gen_fsm))].
