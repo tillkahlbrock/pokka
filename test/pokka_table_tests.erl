4ddea64bd8f0d1d0a21c80ed95f37ce7686cd1a1
Till Kahlbrock <till.kahlbrock@gmx.com>
2013-01-08 06:01:58 +0100

    more table unit tests

--- test/pokka_table_tests.erl
+++ test/pokka_table_tests.erl
@@ -11,7 +11,7 @@
 join_new_player_test_() ->
   [
     {"it should add a player to the empty players list",
-    ?setup(#state{}, first_join_in_idle_state)},
+    ?setup(first_join_in_idle_state)},
     {"it should add a second player to the players list and keep the existing one",
     ?setup(#state{players=[{player1, ?SOME_PID}]}, second_join_in_idle_state)},
     {"it should send a notification to the player when a new player joins",
@@ -21,7 +21,9 @@ join_new_player_test_() ->
     {"it should remove the player from player list on leave",
     ?setup(#state{players=[{player1, ?SOME_PID}, {player2, ?SOME_PID}]}, remove_player_on_leave)},
     {"it should send a notification to the player when a player leaves",
-    ?setup(#state{players=[{player1, ?SOME_PID}, {player2, ?SOME_PID}]}, send_notification_to_players_on_leave)}
+    ?setup(#state{players=[{player1, ?SOME_PID}, {player2, ?SOME_PID}]}, send_notification_to_players_on_leave)},
+    {"it should stop after receiving a terminate message",
+    ?setup(stop_after_terminate_message)}
   ].
 
 %%%%%%%%%%%%%%%%%%%%%%%
@@ -67,3 +69,7 @@ send_notification_to_players_on_leave(InitStateData = #state{players=[_Player1, 
   meck:expect(gen_fsm, send_all_state_event, fun(Pid, Message) -> {ok, Pid, Message} end),
   pokka_table:handle_event({leave, Player2}, some_state, InitStateData),
   [?_assert(meck:validate(gen_fsm))].
+
+stop_after_terminate_message(InitStateData) ->
+  Result = pokka_table:handle_sync_event(terminate, sender, some_state, InitStateData),
+  [?_assertEqual({stop, normal, ok, InitStateData}, Result)].
