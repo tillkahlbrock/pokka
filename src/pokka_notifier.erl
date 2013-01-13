c6ef05733b8e04786288ed07f16dad282a5ee2ee
Till Kahlbrock <till.kahlbrock@gmx.com>
2013-01-13 08:12:59 +0100

    adds notifier:leave

--- src/pokka_notifier.erl
+++ src/pokka_notifier.erl
@@ -1,8 +1,14 @@
 -module(pokka_notifier).
--export([join/2]).
+-export([join/2, leave/2]).
 
 join([], _NewPlayer) -> ok;
 
 join([{_Player, Pid} | Rest], NewPlayer) ->
   gen_fsm:send_event(Pid, {status, "status: new player " ++ atom_to_list(NewPlayer) ++ " joined the table"}),
   join(Rest, NewPlayer).
+
+leave([], _NewPlayer) -> ok;
+
+leave([{_Player, Pid} | Rest], NewPlayer) ->
+  gen_fsm:send_event(Pid, {status, "status: player " ++ atom_to_list(NewPlayer) ++ " left the table"}),
+  leave(Rest, NewPlayer).
--- test/pokka_notifier_tests.erl
+++ test/pokka_notifier_tests.erl
@@ -2,6 +2,7 @@
 -include_lib("eunit/include/eunit.hrl").
 -define(setup(PlayerList, Test), {setup, fun() -> start(PlayerList) end, fun stop/1, fun Test/1}).
 -define(join_msg(NewPlayer), {status, "status: new player " ++ atom_to_list(NewPlayer) ++ " joined the table"}).
+-define(leave_msg(NewPlayer), {status, "status: player " ++ atom_to_list(NewPlayer) ++ " left the table"}).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% TESTS DESCRIPTIONS %%%
@@ -9,11 +10,21 @@
 send_message_on_join_test_() ->
   [
     {"It should send no message if the player list is empty",
-    ?setup([], no_message_send_on_empty_list)},
+    ?setup([], join_no_message_send_on_empty_list)},
     {"It should send a message to the player in the list",
-    ?setup([{player1, pid1}], send_message_to_player)},
+    ?setup([{player1, pid1}], join_send_message_to_player)},
     {"It should send a message to all the players in the list",
-    ?setup([{player1, pid1}, {player2, pid2}], send_message_to_players)}
+    ?setup([{player1, pid1}, {player2, pid2}], join_send_message_to_players)}
+  ].
+
+send_message_on_leave_test_() ->
+  [
+    {"It should send no message if the player list is empty",
+    ?setup([], leave_no_message_send_on_empty_list)},
+    {"It should send a message to the player in the list",
+    ?setup([{player1, pid1}], leave_send_message_to_player)},
+    {"It should send a message to all the players in the list",
+    ?setup([{player1, pid1}, {player2, pid2}], leave_send_message_to_players)}
   ].
 
 %%%%%%%%%%%%%%%%%%%%%%%
@@ -32,7 +43,7 @@ stop(_PlayerList) ->
 %%%%%%%%%%%%%%%%%%%%
 
 %% Player joins
-no_message_send_on_empty_list(PlayerList) ->
+join_no_message_send_on_empty_list(PlayerList) ->
   meck:expect(gen_fsm, send_event, fun(_Pid, _Message) -> ok end),
   pokka_notifier:join(PlayerList, some_player),
   [
@@ -40,7 +51,7 @@ no_message_send_on_empty_list(PlayerList) ->
     ?_assertEqual([], meck:history(gen_fsm))
   ].
 
-send_message_to_player(PlayerList = [{_Name, Pid}]) ->
+join_send_message_to_player(PlayerList = [{_Name, Pid}]) ->
   meck:expect(gen_fsm, send_event, fun(_P, _M) -> ok end),
   NewPlayer = some_new_player,
   pokka_notifier:join(PlayerList, NewPlayer),
@@ -49,7 +60,7 @@ send_message_to_player(PlayerList = [{_Name, Pid}]) ->
     ?_assert(meck:called(gen_fsm, send_event, [Pid, ?join_msg(NewPlayer)]))
   ].
 
-send_message_to_players(PlayerList = [{_Name1, Pid1}, {_Name2, Pid2}]) ->
+join_send_message_to_players(PlayerList = [{_Name1, Pid1}, {_Name2, Pid2}]) ->
   meck:expect(gen_fsm, send_event, fun(_P, _M) -> ok end),
   NewPlayer = some_new_player,
   pokka_notifier:join(PlayerList, NewPlayer),
@@ -57,4 +68,32 @@ send_message_to_players(PlayerList = [{_Name1, Pid1}, {_Name2, Pid2}]) ->
     ?_assert(meck:validate(gen_fsm)),
     ?_assert(meck:called(gen_fsm, send_event, [Pid1, ?join_msg(NewPlayer)])),
     ?_assert(meck:called(gen_fsm, send_event, [Pid2, ?join_msg(NewPlayer)]))
+  ].
+
+%% Player leaves
+leave_no_message_send_on_empty_list(PlayerList) ->
+  meck:expect(gen_fsm, send_event, fun(_Pid, _Message) -> ok end),
+  pokka_notifier:leave(PlayerList, some_player),
+  [
+    ?_assert(meck:validate(gen_fsm)),
+    ?_assertEqual([], meck:history(gen_fsm))
+  ].
+
+leave_send_message_to_player(PlayerList = [{_Name, Pid}]) ->
+  meck:expect(gen_fsm, send_event, fun(_P, _M) -> ok end),
+  NewPlayer = some_new_player,
+  pokka_notifier:leave(PlayerList, NewPlayer),
+  [
+    ?_assert(meck:validate(gen_fsm)),
+    ?_assert(meck:called(gen_fsm, send_event, [Pid, ?leave_msg(NewPlayer)]))
+  ].
+
+leave_send_message_to_players(PlayerList = [{_Name1, Pid1}, {_Name2, Pid2}]) ->
+  meck:expect(gen_fsm, send_event, fun(_P, _M) -> ok end),
+  NewPlayer = some_new_player,
+  pokka_notifier:leave(PlayerList, NewPlayer),
+  [
+    ?_assert(meck:validate(gen_fsm)),
+    ?_assert(meck:called(gen_fsm, send_event, [Pid1, ?leave_msg(NewPlayer)])),
+    ?_assert(meck:called(gen_fsm, send_event, [Pid2, ?leave_msg(NewPlayer)]))
   ].
