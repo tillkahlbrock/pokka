605b5a6f4786059a53cbc876eed3424201196ff3
Till Kahlbrock <till.kahlbrock@gmx.com>
2013-09-30 14:37:59 +0200

    adjusts testplayer

--- test/pokka_test_player.erl
+++ test/pokka_test_player.erl
@@ -1,8 +1,11 @@
 -module(pokka_test_player).
+
+%% API
 -export([start/0]).
 
 start() ->
+  {_Status, _Socket} = gen_tcp:connect('localhost', 12345, [binary, {packet, 0}]),
   receive
-    Message -> erlang:display(Message)
-  end,
-  start().
+    {tcp, _Socket, M1} -> M1
+  after 2000 -> "received timeout while waiting for greeting."
+  end.
